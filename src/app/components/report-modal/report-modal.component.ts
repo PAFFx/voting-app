4e58ac664c5d3802756f224d3d9896ed9a47ac7e
Jittabun Banrerngsri <jj2jom@gmail.com>
2024-04-03 18:31:18 +0700

    connect report modal to description

--- src/app/components/report-modal/report-modal.component.ts
+++ src/app/components/report-modal/report-modal.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
-import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap'
+import {
+  NgbModal,
+  NgbModalOptions,
+  NgbActiveModal,
+} from '@ng-bootstrap/ng-bootstrap'
 
 import { TopicDetail } from '../topic-detail/topic-detail.component'
 
@@ -9,4 +13,20 @@ import { TopicDetail } from '../topic-detail/topic-detail.component'
   templateUrl: './report-modal.component.html',
   styleUrls: ['./report-modal.component.scss'],
 })
-export class ReportModal { }
+export class ReportModal {
+  constructor(
+    public modal: NgbModal,
+    public activeModal: NgbActiveModal
+  ) { }
+
+  closeReportModal() {
+    this.activeModal.dismiss()
+  }
+
+  openDetailModal() {
+    const options: NgbModalOptions = {
+      animation: false,
+    }
+    this.modal.open(TopicDetail, options)
+  }
+}
