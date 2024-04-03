908753aa8c7cd996b1cdd25054ed5636432b6367
Jittabun Banrerngsri <jj2jom@gmail.com>
2024-04-03 16:13:55 +0700

    finish vote submit modal ui

--- src/app/components/vote-modal/vote-modal.component.ts
+++ src/app/components/vote-modal/vote-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core'
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog'
 
 import { VoteOptionItem } from '../vote-option-item/vote-option-item.component'
@@ -8,10 +9,14 @@ import { VoteOptionItem } from '../vote-option-item/vote-option-item.component'
   standalone: true,
   templateUrl: './vote-modal.component.html',
   styleUrls: ['./vote-modal.component.scss'],
-  imports: [MatDialogModule, VoteOptionItem],
+  imports: [MatDialogModule, CommonModule, VoteOptionItem],
 })
 export class VoteModal {
   constructor(public modalRef: MatDialogRef<VoteModal>) { }
 
   options: string[] = ['Option1', 'Option2', 'Option3', 'Option4', 'Option5']
+
+  closeModal() {
+    this.modalRef.close()
+  }
 }
