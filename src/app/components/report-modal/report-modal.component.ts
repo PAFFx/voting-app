b476cdf0856f702decc21683ba901fbc948eb34c
Jittabun Banrerngsri <jj2jom@gmail.com>
2024-04-03 17:30:50 +0700

    start report modal

---
+++ src/app/components/report-modal/report-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'report-modal',
+  standalone: true,
+  templateUrl: './report-modal.component.html',
+  styleUrls: ['./report-modal.component.scss'],
+})
+export class reportModal { }
