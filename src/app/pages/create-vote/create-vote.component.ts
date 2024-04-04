fcec54ca211666df9819ecd86e5cbb670badb1f0
Jittabun Banrerngsri <jj2jom@gmail.com>
2024-04-04 12:53:18 +0700

    add icon and create-option

--- src/app/pages/create-vote/create-vote.component.ts
+++ src/app/pages/create-vote/create-vote.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 import { NavBar } from '../../components/nav-bar/nav-bar.component'
+import { BootstrapIcons, BootstrapIconsModule } from 'ng-bootstrap-icons'
 
 @Component({
   selector: 'create-vote',
   standalone: true,
   templateUrl: './create-vote.component.html',
   styleUrl: './create-vote.component.scss',
-  imports: [NavBar],
+  imports: [NavBar, CommonModule],
 })
-export class CreateVote { }
+export class CreateVote {
+  optionForms: string[] = ['']
+
+  addOption() {
+    this.optionForms.push('')
+  }
+}
